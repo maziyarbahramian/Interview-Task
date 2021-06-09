2ae360f0e43a74608a525ee2b19b139497966d94
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-06-09 18:24:38 +0430

    add floating action buttons

--- app/src/main/java/com/maziyar/interview/ui/main/MainFragment.kt
+++ app/src/main/java/com/maziyar/interview/ui/main/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.viewModels
 import com.maziyar.interview.R
 import com.maziyar.interview.databinding.FragmentMainBinding
@@ -20,6 +22,33 @@ class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var isClickable = false
+
+    private val rotateOpen: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            context,
+            R.anim.rotate_open_anim
+        )
+    }
+    private val rotateClose: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            context,
+            R.anim.rotate_close_anim
+        )
+    }
+    private val fromBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            context,
+            R.anim.from_bottom_anim
+        )
+    }
+    private val toBottom: Animation by lazy {
+        AnimationUtils.loadAnimation(
+            context,
+            R.anim.to_bottom_anim
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,12 +58,66 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupFloatingActionButtons()
+    }
 
+    private fun setupFloatingActionButtons() {
+        binding.apply {
+            fabAdd.setOnClickListener {
+                onFabAddButtonClicked()
+            }
+
+            fabAddFolder.setOnClickListener {
+                Log.i(TAG, "add folder button clicked")
+            }
+
+            fabAddNote.setOnClickListener {
+                Log.i(TAG, "add note button clicked")
+            }
+        }
+    }
+
+    private fun onFabAddButtonClicked() {
+        setFabButtonsVisibility(isClickable)
+        setFabButtonsAnimation(isClickable)
+        setFabButtonsClickable(isClickable)
+        isClickable = !isClickable
+    }
+
+
+    private fun setFabButtonsVisibility(isClickable: Boolean) {
+        if (!isClickable) {
+            binding.fabAddNote.visibility = View.VISIBLE
+            binding.fabAddFolder.visibility = View.VISIBLE
+        } else {
+            binding.fabAddNote.visibility = View.INVISIBLE
+            binding.fabAddFolder.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setFabButtonsAnimation(isClickable: Boolean) {
+        if (!isClickable) {
+            binding.fabAddNote.startAnimation(fromBottom)
+            binding.fabAddFolder.startAnimation(fromBottom)
+            binding.fabAdd.startAnimation(rotateOpen)
+        } else {
+            binding.fabAddNote.startAnimation(toBottom)
+            binding.fabAddFolder.startAnimation(toBottom)
+            binding.fabAdd.startAnimation(rotateClose)
+        }
+    }
+
+    private fun setFabButtonsClickable(isClickable: Boolean) {
+        binding.apply {
+            fabAddFolder.isClickable = !isClickable
+            fabAddNote.isClickable = !isClickable
+        }
     }
 
     override fun onDestroyView() {
